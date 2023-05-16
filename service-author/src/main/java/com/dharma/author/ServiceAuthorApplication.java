067554a84d6581476bf1b5d0c3b80394be1ad603
luis <laliagam@unsa.edu.pe>
2023-05-16 11:04:37 -0500

    add server registry and change pom files with dependencies and add anotations

--- service-author/src/main/java/com/dharma/author/ServiceAuthorApplication.java
+++ service-author/src/main/java/com/dharma/author/ServiceAuthorApplication.java
@@ -2,8 +2,10 @@ package com.dharma.author;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceAuthorApplication {
 
 	public static void main(String[] args) {
